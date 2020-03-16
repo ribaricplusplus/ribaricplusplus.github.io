2f173cc9e64daf9fa3e437c828eacd423a27de73
ribaricplusplus <bruno.ribaric@photomath.net>
2020-03-16 21:18:43 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    plugins: [
+      PnpWebpackPlugin,
+    ],
+  },
+  resolveLoader: {
+    plugins: [
+      PnpWebpackPlugin.moduleLoader(module),
+    ],
+  },
+  target: 'web',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules: [
+          {
+              use: [
+                  {
+                      loader: 'babel-loader',
+                      options: {
+                          presets: [['@babel/preset-env', {
+                              corejs: '3.6',
+                              targets: '> 0.25% and not dead',
+                              useBuiltIns: 'entry'
+                          }], '@babel/preset-react']
+                      }
+                  }
+              ]
+          }
+      ]
+  }
+};
