d3797fc595de6bf95f41b240ec9e98c3281c6279
ribaricplusplus <bruno.ribaric@photomath.net>
2020-03-18 03:06:22 +0100

    Basic website done

--- src/app.js
+++ src/app.js
@@ -1,5 +1,47 @@
 import React from 'react'
 
 export default function App(){
-    <div className='something'>Hello world!</div>
+    return (<main>
+        <header>
+            <h1>Bruno Ribarić</h1>
+            <h2>Front-end web developer</h2>
+        </header>
+        <section>
+            <h1>
+                About
+            </h1>
+            <p>
+                Working as a web developer for <a href="https://photomath.net/en/">Photomath</a>. Recently
+                starting to explore freelancing opportunities.
+            </p>
+        </section>
+        <section>
+            <h1>
+                Interests
+            </h1>
+            <ul>
+                <li>Build user interfaces and integrate them with back-ends or other APIs</li>
+                <li>Develop chrome extensions</li>
+                <li>Write technical specifications and design software architecture</li>
+                <li>Collaborate using Git, GitHub, Slack, Jira, and other similar tools</li>
+            </ul>
+        </section>
+        <section>
+            <h1>
+                Skills
+            </h1>
+            <ul>
+                <li><strong>Basics:</strong>HTML5, CSS, Sass, JavaScript</li>
+                <li><strong>Frameworks:</strong>React, Semantic UI</li>
+                <li><strong>JavaScript tools:</strong>Webpack, Babel, core-js, yarn, ESLint, ...</li>
+                <li><strong>Other:</strong>Git, GitHub, Slack, figma ...</li>
+            </ul>
+        </section>
+        <section>
+            <h1>
+                Contact
+            </h1>
+            <p>Email: br.ribaric@gmail.com</p>
+        </section>
+    </main>)
 }
