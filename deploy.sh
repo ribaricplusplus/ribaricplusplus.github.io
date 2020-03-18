a05a5968727fff5ae9ffae04f8eff1deac7545f5
ribaricplusplus <bruno.ribaric@photomath.net>
2020-03-18 03:54:10 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 yarn build-all
 git checkout --orphan new-master
 git rm --cached -rf .
-rm .gitignore
+printf "*\n" > .gitignore
+printf "%s\n" "!dist" "!dist/**" "!index.html" "!.gitignore"  >> .gitignore
+git add .gitignore
+git commit -m "Add gitignore"
 git add dist index.html
 git commit -m "Deploy"
 git branch -D master
 git branch -m master
-#git push -f origin master
-#git checkout source
+git push -f origin master
+git checkout source
